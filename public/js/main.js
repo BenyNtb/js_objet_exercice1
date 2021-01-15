d921bfcfaf6c7473862a076e88b85536d753de48
Beny <b.ntambwe97@gmail.com>
2021-01-15 10:41:34 +0100

    almost done

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+// EXO 1
+// let me = {
+//     nom1 : 'Ntb',
+//     prenom : 'Beny',
+//     age1 : 23,
+//     taille : '165cm',
+    // hi(){
+    //     console.log(`Hi my name is ${this.prenom} ${this.nom} and I'm ${this.age}`)
+    // }
+    
+// }
+
+// me.hi()
+
+
+// EXO 2
+
+// let you = {
+//     nom2 : 'Doe',
+//     prenom2 : 'John',
+//     age2 : 32,
+//     taille : '195cm',
+
+// }
+
+// let them = {
+//     nom3 : ' ',
+//     prenom : ' ',
+//     age : 20,
+//     taille : ' ',
+// }
+
+// EXO 3
+// them.hello = function(){ 
+//         console.log(`${this.nom1} and ${this.age2}`);
+//     }
+//     them.hello()
+
+// EXO 4
+
+// let us = {
+//     nom : 'Doe',
+//     prenom : 'Jane',
+//     age : 26,
+//     taille : '175cm',
+//     coucou(){
+//         console.log(`Hi my name is ${this.prenom2}`)
+//     }
+    
+// }
+// us.coucou()
+
+// EXO 5
+
+// let perso = {
+//     nom : 'Ntb',
+//     prenom : 'Beny',
+//     hi(){
+//         console.log(`Hi my name is ${this.prenom} ${this.nom} `);
+// }
+// }
+// perso.hi()
+
+// EXO 6
+
+// let you = prompt('How old are you ?')
+// let object = {
+//     nom : 'Beny',
+//     age : "",
+//     hello(){
+//         alert(`${this.nom} is ${this.you}`);
+//     }
+// }
+// object.hello();
+
+// EXO 7
+
+
+// EXO 8
+
+// EXO 9
